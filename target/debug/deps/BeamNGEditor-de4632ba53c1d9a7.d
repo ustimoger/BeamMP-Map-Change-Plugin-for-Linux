55592be4388a0f5a0d5ff9606b9270628602523e
ustimoger <ustimoger@gmail.com>
2023-10-19 15:48:16 +0200

    cleaned up

--- target/debug/deps/BeamNGEditor-de4632ba53c1d9a7.d
+++
@@ -1,5 +0,0 @@
-/Users/timohess/Downloads/maincpy/target/debug/deps/BeamNGEditor-de4632ba53c1d9a7.rmeta: src/main.rs
-
-/Users/timohess/Downloads/maincpy/target/debug/deps/BeamNGEditor-de4632ba53c1d9a7.d: src/main.rs
-
-src/main.rs:
